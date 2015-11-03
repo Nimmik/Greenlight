c62dd9172ab44829f6f4020ae10c27ab158028d2
Min-Woo Kim <mwkim1992@live.com>
2015-11-03 21:24:13 +0900

    initialize

---
+++ GreenLight/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using GreenLight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JBWebappLibrary.Repository;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Greenlight.Models.Context;
+
+namespace GreenLight.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected IUnitOfWork unitOfWork;
+
+        public BaseController()
+        {
+            unitOfWork = new UnitOfWork(new DefaultContext());
+        }
+    }
+}
---
+++ GreenLight/Models/Context/DefaultContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Greenlight.Models.Context
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
