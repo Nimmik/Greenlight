0ef7e526253d690512662e284b53000000635b03
mtbnunu <mtbnunu@gmail.com>
2015-11-03 18:51:37 -0800

    restructure content

--- GreenLight/App_Start/BundleConfig.cs
+++ GreenLight/App_Start/BundleConfig.cs
@@ -9,23 +9,23 @@ namespace GreenLight
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/js/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/js/jquery.validate*"));
 
             // Modernizr의 개발 버전을 사용하여 개발하고 배우십시오. 그런 다음
             // 프로덕션할 준비가 되면 http://modernizr.com 링크의 빌드 도구를 사용하여 필요한 테스트만 선택하십시오.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/js/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/js/bootstrap.js",
+                      "~/Content/js/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap/bootstrap.min.css",
+                      "~/Content/css/site.min.css"));
         }
     }
 }
